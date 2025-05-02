8ec45d23763ec4527867c69e28cbfe0bea6dea25
望尘空忧 <wckfxxjstdwckyxphdhqxg@outlook.com>
2025-05-02 15:22:16 +0800

    · 完成深浅色主题的适配。 · 移除多余的日志打印。

--- OHOS_Project/entry/src/main/cpp/types/libentry/Index.d.ts
+++ OHOS_Project/entry/src/main/cpp/types/libentry/Index.d.ts
@@ -1,5 +1,9 @@
 export const setStartDocumentViewPicker: (cbFn: (maxSelectNumber: number, defaultFilePathUri: string,
-  fileSuffixFilters: Array<string>, selectMode: number, authMod: boolean) =>  void) => void;
+  fileSuffixFilters: Array<string>, selectMode: number, authMod: boolean) => void) => void;
+
 export const setStartDocumentViewPickerSaveMode: (cbFn: (newFileNames: Array<string>, defaultFilePathUri: string,
-  fileSuffixChoices: Array<string>, pickerMode: number) =>  void) => void;
+  fileSuffixChoices: Array<string>, pickerMode: number) => void) => void;
+
 export const setPickerResult: (result: Array<string>) => void;
+
+export const setColor: (colorMode: number) => void;
--- Src/Avalonia.OpenHarmony/OpenHarmonyPlatformSettings.cs
+++ Src/Avalonia.OpenHarmony/OpenHarmonyPlatformSettings.cs
@@ -1,8 +1,133 @@
-﻿using Avalonia.Platform;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using Avalonia.Platform;
+using Avalonia.Threading;
+using OpenHarmony.NDK.Bindings.Native;
 
 namespace Avalonia.OpenHarmony;
 
 public class OpenHarmonyPlatformSettings : DefaultPlatformSettings
 {
-    
+    public OpenHarmonyPlatformSettings()
+    {
+        OHDebugHelper.Debug("OpenHarmonyPlatformSettings 被创建了");
+        ColorModeChanged += mode =>
+        {
+            OHDebugHelper.Debug("OpenHarmonyPlatformSettings" + $"ColorModeChanged: {mode}");
+            OnColorValuesChanged(new PlatformColorValues()
+            {
+                ThemeVariant = mode switch
+                {
+                    ColorMode.COLOR_MODE_DARK => PlatformThemeVariant.Dark,
+                    _ => PlatformThemeVariant.Light
+                }
+            });
+        };
+        OnColorValuesChanged(new PlatformColorValues()
+        {
+            ThemeVariant = _colorMode switch
+            {
+                ColorMode.COLOR_MODE_DARK => PlatformThemeVariant.Dark,
+                _ => PlatformThemeVariant.Light
+            }
+        });
+    }
+
+    private static event Action<ColorMode>? ColorModeChanged;
+    private static ColorMode _colorMode;
+
+    [UnmanagedCallersOnly(CallConvs = [typeof(CallConvCdecl)], EntryPoint = "setColor")]
+    public static unsafe napi_value SetColor(napi_env env, napi_callback_info info)
+    {
+        ulong argc = 1;
+        var args = stackalloc napi_value[(int)argc];
+        node_api.napi_get_cb_info(env, info, &argc, args, null, null);
+        int result;
+        if (node_api.napi_get_value_int32(env, args[0], &result) is napi_status.napi_ok)
+        {
+            if (result is >= -1 and <= 1)
+            {
+                ColorModeChanged?.Invoke(_colorMode = (ColorMode)result);
+            }
+            else
+            {
+                ColorModeChanged?.Invoke(_colorMode = ColorMode.COLOR_MODE_NOT_SET);
+            }
+        }
+
+        return default;
+    }
+}
+
+internal enum ColorMode
+{
+    /**
+     * The color mode is not set.
+     *
+     * @syscap SystemCapability.Ability.AbilityBase
+     * @since 9
+     */
+    /**
+     * The color mode is not set.
+     *
+     * @syscap SystemCapability.Ability.AbilityBase
+     * @crossplatform
+     * @since 10
+     */
+    /**
+     * The color mode is not set.
+     *
+     * @syscap SystemCapability.Ability.AbilityBase
+     * @crossplatform
+     * @atomicservice
+     * @since 11
+     */
+    COLOR_MODE_NOT_SET = -1,
+
+    /**
+     * Dark mode.
+     *
+     * @syscap SystemCapability.Ability.AbilityBase
+     * @since 9
+     */
+    /**
+     * Dark mode.
+     *
+     * @syscap SystemCapability.Ability.AbilityBase
+     * @crossplatform
+     * @since 10
+     */
+    /**
+     * Dark mode.
+     *
+     * @syscap SystemCapability.Ability.AbilityBase
+     * @crossplatform
+     * @atomicservice
+     * @since 11
+     */
+    COLOR_MODE_DARK = 0,
+
+    /**
+     * Light mode.
+     *
+     * @syscap SystemCapability.Ability.AbilityBase
+     * @since 9
+     */
+    /**
+     * Light mode.
+     *
+     * @syscap SystemCapability.Ability.AbilityBase
+     * @crossplatform
+     * @since 10
+     */
+    /**
+     * Light mode.
+     *
+     * @syscap SystemCapability.Ability.AbilityBase
+     * @crossplatform
+     * @atomicservice
+     * @since 11
+     */
+    COLOR_MODE_LIGHT = 1
 }
